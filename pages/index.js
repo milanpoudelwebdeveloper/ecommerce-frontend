58602e3d6910a43e8dea494a1ba9a3b30cdc428d
Milan Poudel <milanpoudel.safalza@gmail.com>
2022-06-08 20:45:32 +0545

    fix/added-shop-icon-and-link

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,6 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Checking deployment</h1>
       <NewArrivals totalProductsCount={totalProductsCount} />
       <BestSellers totalProductsCount={totalProductsCount} />
       <CategoriesList />
