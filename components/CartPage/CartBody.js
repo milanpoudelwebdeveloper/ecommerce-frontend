555f6e8586476aa78bb9efc037711b7fc3e74ca4
Milan Poudel <milanpoudel.safalza@gmail.com>
2022-06-12 16:06:21 +0545

    feature/count-or-quantity-update-in-cart

--- components/CartPage/CartBody.js
+++ components/CartPage/CartBody.js
@@ -7,10 +7,15 @@ import { addToCart } from '../../app/cartSlice'
 const CartBody = ({ cart }) => {
   const dispatch = useDispatch()
   const handleColorChange = (e, productId) => {
+    //first we assign all cart items to new array
     let cartItems = [...cart]
     cart.map((item) => {
+      //after mapping
       if (item._id === productId) {
+        //we will find arrayIndex of that item which matches with productid we send
         let arrayIndex = cartItems.indexOf(item)
+
+        //we will update that particular item with the help of index
         cartItems[arrayIndex] = {
           ...cartItems[arrayIndex],
           color: e.target.value,
@@ -24,8 +29,26 @@ const CartBody = ({ cart }) => {
         }
       }
     })
-    console.log('hey final cart items are', cartItems)
   }
+
+  const handleCountChange = (e, productId) => {
+    let cartItems = [...cart]
+
+    cart.map((item) => {
+      if (item._id === productId) {
+        let foundElementIndex = cartItems.indexOf(item)
+        cartItems[foundElementIndex] = {
+          ...cartItems[foundElementIndex],
+          count: e.target.value,
+        }
+        dispatch(addToCart(cartItems))
+      }
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem('ecommerce-cart', JSON.stringify(cartItems))
+      }
+    })
+  }
+
   return (
     <>
       {cart?.map(
@@ -60,7 +83,19 @@ const CartBody = ({ cart }) => {
                   ))}
                 </select>
               </td>
-              <td>{count}</td>
+              <td>
+                <input
+                  min={1}
+                  defaultValue={count}
+                  type="number"
+                  onChange={(e) => {
+                    console.log('count is', count)
+                    if (count >= 1) {
+                      handleCountChange(e, _id)
+                    }
+                  }}
+                />
+              </td>
               <td>{shipping}</td>
               <td>Delete</td>
             </tr>
