1d950ca472c404e771d4586ff95d823369f12ea0
Milan Poudel <milanpoudel.safalza@gmail.com>
2022-06-12 11:32:32 +0545

    feature/cart-summary-order-right-side

--- pages/cart.js
+++ pages/cart.js
@@ -1,10 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import CartItems from '../components/CartItems'
+import CartItems from '../components/CartPage/CartItems'
+import { getTotalPrice } from '../utils/getTotalPrice'
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart)
+
+  const user = useSelector((state) => state.user)
 
   return (
     <div className="container-fluid">
@@ -24,7 +27,30 @@ const Cart = () => {
             <CartItems cart={cartItems} />
           )}
         </div>
-        <div className="col-md-4">Order Summary</div>
+        <div className="col-md-4">
+          <h4>Order Summary</h4>
+          <hr />
+          <p>Products</p>
+          {cartItems.map(({ title, count, price }, i) => (
+            <div key={i}>
+              <p>
+                {title} X {count} = ${price * count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          Total: <b>${getTotalPrice(cartItems)}</b>
+          <hr />
+          {user ? (
+            <button className="btn btn-sm btn-primary mt-2">
+              Proceed to checkout
+            </button>
+          ) : (
+            <button className="btn btn-sm btn-primary mt-2">
+              Login to checkout
+            </button>
+          )}
+        </div>
       </div>
     </div>
   )
