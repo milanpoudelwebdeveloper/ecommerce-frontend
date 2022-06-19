284775be16a7a4dbf9851fb02c7935fb9533f962
milan poudel <milanwebdeveloper1@gmail.com>
2022-06-19 15:54:55 +0545

    feature/use-react-query-in-all-api-requests-of-homepage

--- pages/category/[id].tsx
+++ pages/category/[id].tsx
@@ -1,55 +1,76 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 import { getCategory } from '../../apiFunctions/category'
 import ProductCardHome from '../../components/ProductCardHome'
 import { NextPage } from 'next'
+import { useQuery } from 'react-query'
 
 const CategoryInfo: NextPage = () => {
-  //fetching all products from that category
-  const [category, setCategory] = useState<any>([])
-  const [allProducts, setAllProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
   const router = useRouter()
 
   const {
     query: { id },
   } = router
 
-  useEffect(() => {
-    loadCategoryInfo()
-  }, [router.isReady])
+  const loadProductsAndCategoryInfo = async () => {
+    const response = await getCategory(id as string)
+    return response.data
+  }
 
-  const loadCategoryInfo = async () => {
-    setLoading(true)
-    try {
-      const response = await getCategory(id as string)
-      const category = response.data.category
-      const allProducts = response.data.allProducts
-      setCategory(category)
-      setAllProducts(allProducts)
-      setLoading(false)
-    } catch (e) {
-      console.log(e)
-      setLoading(false)
+  const query = ['information', id]
+
+  const { isLoading, error, data } = useQuery(
+    query,
+    loadProductsAndCategoryInfo,
+    {
+      enabled: !!id,
+      refetchOnWindowFocus: false,
     }
+  )
+
+  //without query
+
+  // useEffect(() => {
+  //   loadCategoryInfo()
+  // }, [router.isReady])
+
+  // const loadCategoryInfo = async () => {
+  //   setLoading(true)
+  //   try {
+  //     const response = await getCategory(id as string)
+  //     const category = response.data.category
+  //     const allProducts = response.data.allProducts
+  //     setCategory(category)
+  //     setAllProducts(allProducts)
+  //     setLoading(false)
+  //   } catch (e) {
+  //     console.log(e)
+  //     setLoading(false)
+  //   }
+  // }
+
+  if (error) {
+    console.log("error while fetching category and it's products", error)
+    return <div>Error when fetching data</div>
   }
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col">
-          {loading ? (
+          {isLoading ? (
             <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
               Loading
             </h4>
           ) : (
             <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
-              {allProducts?.length} in &quot;{category?.name}&quot; category
+              {data?.allProducts?.length} in &quot;{data?.category?.name}&quot;
+              category
             </h4>
           )}
         </div>
       </div>
       <div className="row">
-        {allProducts.map((p) => (
+        {data?.allProducts.map((p: IProduct) => (
           <div className="col-md-4" key={p._id}>
             <ProductCardHome product={p} />
           </div>
--- pages/subs/[id].tsx
+++ pages/subs/[id].tsx
@@ -1,57 +1,80 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useQuery } from 'react-query'
 import { getSub } from '../../apiFunctions/subCategory'
 import ProductCardHome from '../../components/ProductCardHome'
 
 const SubCategoryInfo: NextPage = () => {
-  const [allSubProducts, setAllSubProducts] = useState<ISubCategory[]>([])
-  const [subCategory, setSubCategory] = useState<ISubCategory>(
-    {} as ISubCategory
-  )
-  const [loading, setLoading] = useState(false)
+  // const [allSubProducts, setAllSubProducts] = useState<ISubCategory[]>([])
+  // const [subCategory, setSubCategory] = useState<ISubCategory>(
+  //   {} as ISubCategory
+  // )
+  // const [loading, setLoading] = useState(false)
   const router = useRouter()
 
   const {
     query: { id },
   } = router
 
-  useEffect(() => {
-    loadInfo()
-  }, [router.isReady])
-
-  const loadInfo = async () => {
-    setLoading(true)
-    try {
-      const response = await getSub(id as string)
-      const subCategory = response.data.subCategory
-      const allSubProducts = response.data.allSubProducts
-      setAllSubProducts(allSubProducts)
-      setSubCategory(subCategory)
-      setLoading(false)
-    } catch (e) {
-      console.log(e)
-      setLoading(false)
-    }
+  const loadSubInfoAndItsProducts = async () => {
+    const response = await getSub(id as string)
+    return response.data
   }
+
+  const query = ['subs-products', id]
+
+  const { isLoading, error, data } = useQuery(
+    query,
+    loadSubInfoAndItsProducts,
+    {
+      enabled: !!id,
+      refetchOnWindowFocus: false,
+    }
+  )
+
+  //without react-query
+  // useEffect(() => {
+  //   loadInfo()
+  // }, [router.isReady])
+
+  // const loadInfo = async () => {
+  //   setLoading(true)
+  //   try {
+  //     const response = await getSub(id as string)
+  //     const subCategory = response.data.subCategory
+  //     const allSubProducts = response.data.allSubProducts
+  //     setAllSubProducts(allSubProducts)
+  //     setSubCategory(subCategory)
+  //     setLoading(false)
+  //   } catch (e) {
+  //     console.log(e)
+  //     setLoading(false)
+  //   }
+  // }
+  if (error) {
+    console.log("error while fetching subCategory and it's products", error)
+    return <div>Error when fetching data</div>
+  }
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col">
-          {loading ? (
+          {isLoading ? (
             <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
               Loading
             </h4>
           ) : (
             <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
-              {allSubProducts?.length} in &quot;{subCategory?.name}&quot;
-              sub-category
+              {data?.allSubProducts?.length} in &quot;{data?.subCategory?.name}
+              &quot; sub-category
             </h4>
           )}
         </div>
       </div>
       <div className="row">
-        {allSubProducts.map((p: any) => (
+        {data?.allSubProducts.map((p: IProduct) => (
           <div className="col-md-4" key={p._id}>
             <ProductCardHome product={p} />
           </div>
